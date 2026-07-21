3414b97c7b50539ac1ff92cb5e022b0902c455ff
Atharv Randive <atharvarandive9@gmail.com>
2026-07-21 16:48:33 +0530

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © 2026 घरचा मंडळ | गणपती बाप्पा मोरया 🙏
+      </p>
+    </footer>
+  );
+}
